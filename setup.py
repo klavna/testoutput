622b99bdcee49cd0a8a770e62d91b73fbae38daf
klavna <100742454+klavna@users.noreply.github.com>
2022-05-04 10:51:51 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = true,
+  name = '546lkji',
+  verison='0.0.1',
+  description='oss-dev my output 1234',
+  author='546lkji',
+  author_email='546lkji@gmail.com', 
+  url = 'https://github.com/klavna/testoutput/new/main',
+  download_url='
+  install_requires=['pytest'],
+  long_description = 'oss-dev output python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: Os Independent",
+  ],
+)
